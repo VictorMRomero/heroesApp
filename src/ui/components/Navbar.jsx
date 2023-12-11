5750f1ed25a0e01138f44e150360c13c91faa3aa
victor <vmrbaez@gmai.com>
2023-12-11 07:38:41 -0700

    Se creo un nuevo componente para generar las tergetas de los heroes, mostrando imagen y nombre

--- src/ui/components/Navbar.jsx
+++ src/ui/components/Navbar.jsx
@@ -51,7 +51,7 @@ export const Navbar = () => {
                 </div>
             </div>
 
-            <div className="navbar-collapse collapse w-100 order-3 dual-collapse2 d-flex justify-content">
+            <div className="navbar-collapse collapse w-100 order-3 dual-collapse2 d-flex justify-content-end">
                 <ul className="navbar-nav ml-auto">
                     <span className="nav-item nav-link text-primary">
                         Victor

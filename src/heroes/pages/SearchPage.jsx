ba3babab1c4185da322b3b6337c718e73d98c9ed
victor <vmrbaez@gmai.com>
2023-12-12 17:29:51 -0700

    Se dio funcionalidad a la pagina Search, implementando los query params, y condicionales para mostrar o no mostrar las alertas

--- src/heroes/pages/SearchPage.jsx
+++ src/heroes/pages/SearchPage.jsx
@@ -1,7 +1,34 @@
-
+import { useLocation, useNavigate } from "react-router-dom";
+import { useForm } from "../../hooks/useForm";
+import { HeroCard } from "../components/HeroCard";
+import queryString from 'query-string';
+import { getHeroByName } from "../helpers";
 
 export const SearchPage = () => {
 
+    const navigate = useNavigate();
+    const location = useLocation();
+    
+    const {q = ''} = queryString.parse( location.search );
+
+    const heroes = getHeroByName(q);
+
+   
+
+   const showSearch = (heroes.length === 0);
+
+    const { searchText, onInputChange} = useForm({
+        searchText: q
+    });
+
+    const onSearchSubmit = (event) => {
+        event.preventDefault();
+
+        
+
+        navigate(`?q=${ searchText}`)
+        
+    };
 
     return (
         <>
@@ -13,13 +40,15 @@ export const SearchPage = () => {
                 <div className="col-5">
                     <h4>Searching</h4>
                     <hr />
-                    <form action="">
+                    <form onSubmit={ onSearchSubmit }>
                         <input 
                         type="text"
                         placeholder="Search a hero"
                         className="form-control"
                         name="searchText"
-                        autoComplete="off" />
+                        autoComplete="off" 
+                        value={ searchText}
+                        onChange={ onInputChange}/>
                     </form>
                     <button className="btn btn-outline-primary mt-2">Search</button>
                 </div>
@@ -28,12 +57,27 @@ export const SearchPage = () => {
                     <h4>Results</h4>
                     <hr />
 
-                    <div className="alert alert-primary">Search a hero</div>   
-                    <div className="alert alert-danger">Theresn no results</div>                
+                    <div className="alert alert-primary" style={{display: q !== '' ? "none" : ''}}>Search a hero</div>   
+                    
+                    <div className="alert alert-danger" style={{display: showSearch ? '' : "none"}}>There is no results with <b>{q}</b></div>     
+                    
+                    
+                    {
+                        heroes.map((hero) => (
+
+                        <HeroCard 
+                            key = {hero.id}
+                            {...hero}
+                        />
+                            
+
+                        ))
+                    }
+                    
+
+
                 </div>
-
             </div>
-
         </>
     )
 }
